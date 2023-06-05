9384a70f061705cc282e0bfabc7fe8c08ee57927
Jonas Ångström <jonas.angstrom@gradientech.se>
2023-06-05 07:51:06 +0200

    added surroundAllllllb

--- plugins/user.lua
+++ plugins/user.lua
@@ -1,4 +1,14 @@
 return {
+  {
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+        require("nvim-surround").setup({
+            -- Configuration here, or leave empty to use defaults
+        })
+    end,
+  },
   -- You can also add new plugins here as well:
   -- Add plugins, the lazy syntax
   -- "andweeb/presence.nvim",
