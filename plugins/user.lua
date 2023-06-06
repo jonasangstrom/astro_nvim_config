bd1a50963285024ac0a6f376c5bb83d7992d92f2
Jonas Ångström <jonas.angstrom@gradientech.se>
2023-06-06 23:04:34 +0200

    formatting

--- plugins/user.lua
+++ plugins/user.lua
@@ -4,9 +4,9 @@ return {
     version = "*", -- Use for stability; omit to use `main` branch for the latest features
     event = "VeryLazy",
     config = function()
-        require("nvim-surround").setup({
-            -- Configuration here, or leave empty to use defaults
-        })
+      require("nvim-surround").setup({
+        -- Configuration here, or leave empty to use defaults
+      })
     end,
   },
   -- You can also add new plugins here as well:
